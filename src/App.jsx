ec2edb532cc04b20469d62489f0cacbc723c06cc
Madhav Roshan <boomair@Madhavs-Air.bbrouter>
2024-02-21 15:41:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("white")
+  const click = color =>{
+    setColor(color)
+  }
+  useEffect(()=>{
+    document.body.style.backgroundColor= color
+  },[color])
+
+  return (
+    <>
+     <div className='container'>
+
+      <h1>Pick a color!</h1>
+      <p>You'll see the background transform</p>
+      <div className="card">
+        <button onClick={() => click("lightblue")}>
+          Blue
+        </button>
+        <button onClick={() => click("lightgreen")}>
+          Green
+        </button>
+        <button onClick={() => click("yellow")}>
+          Yellow
+        </button>
+        <button onClick={() => click("purple")}>
+          Purple
+        </button>
+      </div>
+     </div>
+
+    </>
+  )
+}
+
+export default App
